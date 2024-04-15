b8296c2b199d78da5a662540e204fe9923c7f704
krithicr <krithicrajp.1902101@srec.ac.in>
2024-04-15 17:51:14 +0530

    learning path, plan updated

--- src/Components/Course.jsx
+++ src/Components/Course.jsx
@@ -1,77 +1,84 @@
 import {
-    Button,
-    Card,
-    CardBody,
-    CardHeader,
-    Input,
-    Option,
-    Select,
-    Typography
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Input,
+  Option,
+  Select,
+  Typography,
 } from "@material-tailwind/react";
+import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
 
 export function Course() {
-    const [courseData, setCourseData] = useState({
-        courseName: '',
-       
-        level: 'basic'
-    });
-    const navigate = useNavigate();
+  const [courseData, setCourseData] = useState({
+    courseName: "",
+    level: "basic",
+  });
+  const navigate = useNavigate();
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setCourseData(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
-    };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCourseData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
+  const handleAddTopics = () => {
+    navigate("/topics");
+    axios
+      .post("http://172.18.4.108:1111/course", courseData)
+      .then((response) => {
+        console.log("Course data posted successfully:", response.data);
+        // const learningPlanID = response.data.learningPlanID;
+        // navigate("/path", { state: { learningPlanID: learningPlanID } });
+      })
+      .catch((error) => {
+        console.error("Error posting course data:", error);
+      });
+  };
 
+  return (
+    <Card className=" w-full md:w-3/4 lg:w-2/4 xl:w-2/4 mx-auto ">
+      <CardHeader
+        variant="gradient"
+        color="gray"
+        className="mb-4 grid h-28 place-items-center"
+      >
+        <Typography variant="h5" color="white" className="mb-2">
+          Add Course
+        </Typography>
+      </CardHeader>
+      <CardBody>
+        <form className="container p-6 bg-white rounded-lg">
+          <div className="mb-4">
+            <Input
+              id="courseName"
+              name="courseName"
+              variant="outlined"
+              label="Course Name"
+              onChange={handleChange}
+            />
+          </div>
 
-    const handleAddTopics = () => {
-        navigate("/topics");
-        axios.post('http://localhost:1111/courses/add', courseData)
-        .then(response => {
-            console.log('Course data posted successfully:', response.data);
-        })
-        .catch(error => {
-            console.error('Error posting course data:', error);
-        });
-    };
+          <div className="mb-4">
+            <Select variant="outlined" label="Select Level">
+              <Option value="basic">Basic</Option>
+              <Option value="Intermediate">Intermediate</Option>
+              <Option value="Advance">Advance</Option>
+            </Select>
+          </div>
 
-
-    return (
-        <Card className=" w-full md:w-3/4 lg:w-2/4 xl:w-2/4 mx-auto ">
-            <CardHeader
-                variant="gradient"
-                color="gray"
-                className="mb-4 grid h-28 place-items-center"
-            >
-                <Typography variant="h5" color="white" className="mb-2">
-                    Add Course
-                </Typography>
-            </CardHeader>
-            <CardBody>
-                <form className='container p-6 bg-white rounded-lg'>
-                    <div className='mb-4'>
-                        <Input id="courseName" name="courseName" variant="outlined" label="Course Name" value={courseData.title} onChange={handleChange} />
-                    </div>
-                    
-                    <div className="mb-4">
-                        <Select variant="outlined" label="Select Level">
-                            <Option value='basic'>Basic</Option>
-                            <Option value="Intermediate">Intermediate</Option>
-                            <Option value="Advance">Advance</Option>
-                        </Select>
-                    </div>
-
-                    <div className="mb-4 flex justify-end">
-                        <Button ripple={true} onClick={handleAddTopics}>Add Topics</Button>
-                    </div>
-                </form>
-            </CardBody>
-        </Card>
-    );
+          <div className="mb-4 flex justify-end">
+            <Button ripple={true} onClick={handleAddTopics}>
+              Add Topics
+            </Button>
+          </div>
+        </form>
+      </CardBody>
+    </Card>
+  );
 }
--- src/Components/CreatePlan.jsx
+++ src/Components/CreatePlan.jsx
@@ -1,107 +1,128 @@
 import {
-    Button,
-    Card,
-    CardBody,
-    CardHeader,
-    Input,
-    Option,
-    Select,
-    Typography
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Option,
+  Select,
+  Typography,
 } from "@material-tailwind/react";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import axios from "axios"; // Import axios
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function CreatePlan() {
-    const [description, setDescription] = useState('');
-    const [batch, setBatch] = useState(''); // Set initial state for batch
-    const [type, setType] = useState(''); // Set initial state for type
-    const navigate = useNavigate();
+  const [batchItems, setBatchItems] = useState([]);
+  const [batch, setBatch] = useState({});
+  const [type, setType] = useState("");
+  const navigate = useNavigate();
 
-    const handleChangeBatch = (value) => {
-        setBatch(value);
-    };
-    
-    const handleChangeType = (value) => {
-        setType(String(value)); // Convert value to string before setting state
-    };
-    
+  useEffect(() => {
+    fetchBatchItems();
+  }, []);
 
-    const handleSubmit = (event) => {
-        const formData = {
-          batch: batch,
-          type: type,
-          start_date: event.target.start_date.value,
-          end_date: event.target.end_date.value,
-        };
-        const start = event.target.start_date.value;
-        const end = event.target.end_date.value;
-        const dateChecker = start.replace(/-/g, "") - end.replace(/-/g, "");
-     
-        if (dateChecker < 0) {
-        //   Axios POST request to the learningplans/save endpoint
-          axios.post('http://localhost:1111/learningplans/save', formData)
-              .then(response => {
-                  console.log('Learning plan saved successfully:', response.data);
-                  // Redirect to another page after successful submission
-                //   navigate("/path");
-              })
-              .catch(error => {
-                  console.error('Error saving learning plan:', error);
-              });
-        } else {
-          alert("Enter Correct Dates");
-        }
-     
-        event.preventDefault();
-     
-        // Form data to be sent in the POST request
-     
-        // Reset form fields after submission
-        event.target.reset();
+  const fetchBatchItems = async () => {
+    try {
+      const response = await axios.get("http://172.18.4.243:8078/batch");
+      if (response.status === 200) {
+        setBatchItems(response.data);
+      } else {
+        console.error("Failed to fetch courses.");
+      }
+    } catch (error) {
+      console.error("Error occurred while fetching courses:", error);
+    }
+  };
+
+  const handleChangeBatch = (value) => {
+    let matchedBatch = batchItems.filter((item) => item.batchName === value);
+    setBatch(matchedBatch[0]);
+  };
+
+  const handleChangeType = (value) => {
+    setType(String(value));
+  };
+
+  const handleSubmit = (event) => {
+    console.log(batch.batchId, typeof type);
+    const formData = {
+      batchID: 43,
+      type: type,
     };
 
-    return (
-        <Card className="mt-6 w-full md:w-3/4 lg:w-2/4 xl:w-2/4 mx-auto ">
-            <CardHeader
-                variant="gradient"
-                color="gray"
-                className="mb-4 grid h-28 place-items-center"
+    if (batchItems && type) {
+      axios
+        .post("http://172.18.4.108:1111/learning-plan", formData)
+        .then((response) => {
+          console.log("Learning plan saved successfully:", response.data);
+          const learningPlanID = response.data.learningPlanID;
+          navigate("/path", {
+            state: { learningPlanID: learningPlanID, batch: batch.batchId },
+          });
+        })
+        .catch((error) => {
+          console.error("Error saving learning plan:", error);
+        });
+    } else {
+      alert("Fill All Fields");
+    }
+
+    event.preventDefault();
+
+    event.target.reset();
+  };
+
+  return (
+    <Card className="mt-6 w-full md:w-3/4 lg:w-2/4 xl:w-2/4 mx-auto ">
+      <CardHeader
+        variant="gradient"
+        color="gray"
+        className="mb-4 grid h-28 place-items-center"
+      >
+        <Typography variant="h5" color="white" className="mb-2">
+          Add Learning Plan
+        </Typography>
+      </CardHeader>
+      <CardBody>
+        <form
+          className="container p-6 bg-white rounded-lg"
+          onSubmit={handleSubmit}
+        >
+          <div className="mb-4">
+            <Select
+              required
+              variant="outlined"
+              label="Batch Name"
+              onChange={handleChangeBatch}
             >
-                <Typography variant="h5" color="white" className="mb-2">
-                    Add Learning Plan
-                </Typography>
-            </CardHeader>
-            <CardBody>
-                <form className='container p-6 bg-white rounded-lg' onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <Select variant="outlined" label="Batch Name" id="batch" name="batch" onChange={handleChangeBatch}>
-                            <Select.Option value='batch1'>Batch1</Select.Option>
-                            <Select.Option value="batch2">Batch2</Select.Option>
-                            <Select.Option value="batch3">Batch3</Select.Option>
-                            <Select.Option value="od102">OD102</Select.Option>
-                            <Select.Option value="ow101">OW101</Select.Option>
-                        </Select>
-                    </div>
-                    <div className="mb-4">
-                        <Select variant="outlined" label="Type" id="Type" name="Type" value={type} onChange={handleChangeType}>
-                            <Select.Option value='bootcamp'>BOOTCAMP</Select.Option>
-                            <Select.Option value="on_demand">ON-DEMAND</Select.Option>
-                            <Select.Option value="mandatory">MANDATORY</Select.Option>
-                            <Select.Option value="org_wide">ORG-WIDE</Select.Option>
-                        </Select>
-                    </div>
-                    <div className='mb-4'>
-                        <Input id="start_date" name="start_date" type="date" variant="outlined" label="Start date" shrink={true} />
-                    </div>
-                    <div className='mb-4'>
-                        <Input id="end_date" name="end_date" type="date" variant="outlined" label="End date" shrink={true}/>
-                    </div>
-                    <div className="mb-4 flex justify-end">
-                        <Button ripple={true} type="submit">Add Plan</Button>
-                    </div>
-                </form>
-            </CardBody>
-        </Card>
-    );
+              {batchItems.map((item) => (
+                <Option key={item.batchId} value={item.batchName}>
+                  {item.batchName}
+                </Option>
+              ))}
+            </Select>
+          </div>
+          <div className="mb-4">
+            <Select
+              variant="outlined"
+              label="Type"
+              id="Type"
+              onChange={handleChangeType}
+            >
+              <Select.Option value="bootcamp">BOOTCAMP</Select.Option>
+              <Select.Option value="on_demand">ON-DEMAND</Select.Option>
+              <Select.Option value="mandatory">MANDATORY</Select.Option>
+              <Select.Option value="org_wide">ORG-WIDE</Select.Option>
+            </Select>
+          </div>
+
+          <div className="mb-4 flex justify-end">
+            <Button ripple={true} type="submit">
+              Add Plan
+            </Button>
+          </div>
+        </form>
+      </CardBody>
+    </Card>
+  );
 }
